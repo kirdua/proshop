e3078a7cdd058e6cb3b9d0fef5221f179980e1fe
kirdua <kirdua@gmail.com>
2021-02-11 13:16:12 -0600

    placeorder frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 
 const App = () => {
@@ -25,6 +27,8 @@ const App = () => {
             <Route path='/cart/:id?' component={CartScreen}/>
             <Route path='/profile' component={ProfileScreen}/>
             <Route path='/shipping' component={ShippingScreen}/>
+            <Route path='/payment' component={PaymentScreen}/>
+            <Route path='/placeorder' component={PlaceOrderScreen}/>
           </Container>
         </main>
       <Footer/>
