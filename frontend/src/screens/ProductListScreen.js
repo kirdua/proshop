c8783f66b2c11116b570774fe1cbbcdb9b94f8d3
kirdua <kirdua@gmail.com>
2021-02-27 07:52:55 -0600

    added delete functionality to back and frontend

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,13 +4,20 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts } from '../actions/productActions'
+import { listProducts, deleteProduct } from '../actions/productActions'
 
 const ProductListScreen = ({ history, match }) => {
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList)
     const { loading, error, products } = productList
+
+    const productDelete = useSelector(state => state.productDelete)
+    const { 
+        loading: loadingDelete, 
+        error: errorDelete, 
+        success: successDelete 
+    } = productDelete
 
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
@@ -22,7 +29,7 @@ const ProductListScreen = ({ history, match }) => {
             history.push('/login')
         }
 
-    }, [dispatch, history, userInfo])
+    }, [dispatch, history, userInfo, successDelete])
 
     const createProductHandler = (product) => {
         //CREATE PRODUCT
@@ -30,8 +37,7 @@ const ProductListScreen = ({ history, match }) => {
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure')){
-            //dispatch(deleteUser(id))
-            //DELETE PRODUCTS
+            dispatch(deleteProduct(id))
         }
     }
 
@@ -50,6 +56,8 @@ const ProductListScreen = ({ history, match }) => {
                 </Button>
             </Col>
           </Row>
+          {loadingDelete && <Loader/>}
+          {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
           {loading ? ( 
             <Loader/> 
             ) : error ? ( 
@@ -69,8 +77,8 @@ const ProductListScreen = ({ history, match }) => {
                     {products.map(product => (
                         <tr key={product._id}>
                             <td>{product._id}</td>
-                            <td>${product.name}</td>
-                            <td>{product.price}</td>
+                            <td>{product.name}</td>
+                            <td>${product.price}</td>
                             <td>{product.category}</td>
                             <td>{product.brand}</td>
                             <td>
