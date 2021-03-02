1a2b611b024f6e6806a1c36d6230c0f1f35e47c6
kirdua <kirdua@gmail.com>
2021-03-02 15:54:11 -0600

    rate review and search products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,7 +5,8 @@ import {
     getProductById, 
     getProducts,
     createProduct,
-    updateProduct
+    updateProduct,
+    createProductReview
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -13,6 +14,8 @@ router
     .route('/')
     .get(getProducts)
     .post(protect, admin, createProduct)
+
+router.route('/:id/reviews').post(protect, createProductReview)   
 router
     .route('/:id')
     .get(getProductById)
