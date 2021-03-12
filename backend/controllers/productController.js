c06d33665d48702b2c937fe62bafcabfccaef94d
kirdua <kirdua@gmail.com>
2021-03-12 08:41:47 -0600

    top rated products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -152,7 +152,7 @@ const getTopProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({}).sort({ rating: -1 }).limit(3)
   
     res.json(products)
-  })
+})
 
 export {
     getProducts,
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,7 +6,8 @@ import {
     getProducts,
     createProduct,
     updateProduct,
-    createProductReview
+    createProductReview,
+    getTopProducts
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -15,7 +16,8 @@ router
     .get(getProducts)
     .post(protect, admin, createProduct)
 
-router.route('/:id/reviews').post(protect, createProductReview)   
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)   
 router
     .route('/:id')
     .get(getProductById)
